5b7cea966151e5c98d0ff050cce79cd24e724b27
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2020-03-27 12:37:14 -0500

    first commit

---
+++ src/main/java/escuelaing/arep/secure/webSecure/WebSecureApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.arep.secure.webSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escuelaing/arep/secure/webSecure/WebSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.arep.secure.webSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
